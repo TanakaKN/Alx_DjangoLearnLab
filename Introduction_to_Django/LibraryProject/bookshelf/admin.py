3e03df0bc09cfc799148bab155558122b8ef3ce2
Tanaka Ndopo <tkndopo@yahoo.com>
2025-11-02 15:56:06 +0200

    Customize the admin display to improve the management and visibility of book data

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
+class bookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
 
-admin.site.register(Book)
+admin.site.register(Book, bookAdmin)
