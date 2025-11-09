abb74a47821f19e0a886407470f42d6d6b4a0e71
Tanaka Ndopo <tkndopo@yahoo.com>
2025-11-09 19:47:09 +0200

    added a file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
+from .views import list_books   
 
 urlpatterns = [
-     path("books/", views.list_books, name="list_books"),
+    # Function-based view route
+    path("books/", list_books, name="list_books"),
 
-    # class-based view (DetailView) - uses <int:pk> which DetailView expects by default
+    # Class-based DetailView route
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 ]
-
-
