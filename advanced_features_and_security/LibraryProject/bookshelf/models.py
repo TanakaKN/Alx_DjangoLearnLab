d72bd83aabaab1ec0769ce36daa430d9299b0d5c
TanakaKN <tkndopo@yahoo.com>
2025-11-16 22:30:47 +0200

    Add custom user model and manager for authentication

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,52 @@
+# LibraryProject/bookshelf/models.py
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model required by the assignment.
+    """
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+# optional: keep a Book model here if the checker expects it in the bookshelf app
 class Book(models.Model):
-    title = models.CharField (max_length = 200)
-    author = models.CharField(max_length = 100)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
     def __str__(self):
