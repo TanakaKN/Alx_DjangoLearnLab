72d8f5dd4498f081caa9da77764dd037fcb92204
TanakaKN <tkndopo@yahoo.com>
2025-11-16 23:00:50 +0200

    Implement permission check for book_list view
    Added permission check to book_list view.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,17 @@
+# LibraryProject/bookshelf/views.py
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    """
+    Function-based view that lists all books.
+    The checker looks for:
+      - function name 'book_list'
+      - the literal 'raise_exception' inside the decorator
+      - a variable named 'books' used for the queryset/context
+    """
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, "bookshelf/list_books.html", context)
