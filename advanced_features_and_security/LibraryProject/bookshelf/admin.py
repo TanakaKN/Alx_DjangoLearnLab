ea84d441ac6294f9f954a641744c826013b01c0f
TanakaKN <tkndopo@yahoo.com>
2025-11-16 22:31:34 +0200

    Implement custom user admin and register models
    Added a custom user admin interface with additional fields for date of birth and profile photo. Registered the User model and handled potential double-registration for the Book model.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,27 @@
+# LibraryProject/bookshelf/admin.py
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from .models import Book
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')        
-    search_fields = ('title', 'author')               
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "date_of_birth")
+
+
+# register Book if present
+try:
+    admin.site.register(Book)
+except Exception:
+    # avoid double-registration errors
+    pass
