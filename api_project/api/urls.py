70a2909f27092238f8843f41d00f6216cda98931
Tanaka Ndopo <tkndopo@yahoo.com>
2025-11-23 19:07:08 +0200

    Added an api for Viewset book function and mapped it to the url for access

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from . import views
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', views.BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+   path('books/', views.BookList.as_view(), name='book-list'),
 ]
