286ac584d616e2fe634996e5eb723a37dc4a8ff9
Tanaka Ndopo <tkndopo@yahoo.com>
2025-11-23 22:02:51 +0200

    started a new web development project for api use cases

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+
 
 
 class BookList(generics.ListAPIView):
@@ -9,4 +12,7 @@ class BookList(generics.ListAPIView):
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer   
+    serializer_class = BookSerializer  
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+     
