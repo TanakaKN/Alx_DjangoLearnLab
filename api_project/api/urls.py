2d187c23e1ddad314739e5477dc0792c4c368754
Tanaka Ndopo <tkndopo@yahoo.com>
2025-11-23 18:28:52 +0200

    Added a book function and mapped it to the url for access

---
+++ api_project/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.Book_List, name='book-list'),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def Book_List(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
