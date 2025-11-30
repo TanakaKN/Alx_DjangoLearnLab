4fd6e91b979fa6a0d9684ba72b75ab9ed8f91de1
TanakaKN <tkndopo@yahoo.com>
2025-11-30 16:19:13 +0200

    started a new book library app

---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import date
+
+# Book Serializer
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publication_year', 'author']
+
+    # Validation: publication_year cannot be in the future
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+
+# Nested Book Serializer (for use inside AuthorSerializer)
+class NestedBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publication_year']
+
+
+# Author Serializer (with nested books)
+class AuthorSerializer(serializers.ModelSerializer):
+    books = NestedBookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
