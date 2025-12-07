7ac84a3c1a3d0ca9f09dc07e52f6cb2577294a5f
TanakaKN <tkndopo@yahoo.com>
2025-12-07 20:29:46 +0200

    added CRUD operations for users to be able to interact with posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,14 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
 
-
-    path('posts/', views.PostListView.as_view(), name='posts'),              # list
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),  # create
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),  # detail
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),  # update
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),  # delete
-
-
+    
+    path('post/', views.PostListView.as_view(), name='posts'),  
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     path("posts/", views.post_list, name="posts"),
 
     path("login/", views.UserLoginView.as_view(), name="login"),
