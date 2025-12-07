dd2f6ca09133a9f93a5716d1a3d3f30eef4afe8f
TanakaKN <tkndopo@yahoo.com>
2025-12-07 21:51:41 +0200

    Add PostByTagListView and tag URL for tagged posts

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from .models import Post, Comment
 from taggit.forms import TagWidget
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from taggit.models import Tag   # 👈 NEW
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 
 class PostForm(forms.ModelForm):
@@ -25,3 +31,19 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list_by_tag.html'   # we'll create this template
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        # get tag from URL (slug)
+        tag_slug = self.kwargs.get('tag_slug')
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        # return posts that have this tag
+        return Post.objects.filter(tags__in=[self.tag]).order_by('-published_date').distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 
     path('tag/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
     path('search/', views.search_posts, name='search-posts'),
-
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
 
         # Comments section
     path(
