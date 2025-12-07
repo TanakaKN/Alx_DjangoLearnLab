f2de122c789a8e5b89c440599457938ba426817c
TanakaKN <tkndopo@yahoo.com>
2025-12-07 21:40:10 +0200

    removed the manual tag option for inbuilt django for tagging posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,21 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    published_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField()
+    tags = TaggableManager()  
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        """
-        Tells Django where to redirect after create/update.
-        """
-        return reverse('post-detail', args=[str(self.id)])
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
